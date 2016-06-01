a1bb0b9cb35a1defc2890dc692b057f18f36a538
Osamu Takahashi <nitr@sopranoworks.com>
2016-06-01 19:53:31 +0900

    Modified for new codebook-1.0-SNAPSHOT

--- src/main/scala/com/rustyraven/SbtCodebook.scala
+++ src/main/scala/com/rustyraven/SbtCodebook.scala
@@ -7,8 +7,8 @@ import com.rustyraven.codebook.ProtocolGenerator
 object SbtCodebook extends Plugin {
   val Codebook = config("codebook")
 
-//  val codebookTargetLanguage = SettingKey[String]("Code generation target language")
   val codebookGenerate = TaskKey[Seq[File]]("Generate classes from codebook definitions")
+  val codebookDecoderPackageName = SettingKey[Option[String]]("Decoder code package name")
 
   val codebookSettings = inConfig(Codebook)(Seq(
     sourceDirectory <<= (sourceDirectory in Compile) { _ / "codebook"},
@@ -19,17 +19,10 @@ object SbtCodebook extends Plugin {
     sourceGenerators in Compile <+= (codebookGenerate in Codebook)
   )
 
-  def codebookGenerate(srcFiles:Set[File],targetBaseDir:File,log:Logger):Set[File] = {
-    srcFiles.map(src=>ProtocolGenerator.generate(src,targetBaseDir,"scala")).flatten
-  }
-
   def codebookGeneratorTask:Def.Initialize[Task[Seq[File]]] = Def.task {
     val cachedCompile = FileFunction.cached(streams.value.cacheDirectory / "codebook", FilesInfo.lastModified, FilesInfo.exists) {
       in:Set[File] =>
-        codebookGenerate(
-          in,
-          (scalaSource in Codebook).value,
-          streams.value.log)
+        ProtocolGenerator.generate(in,(scalaSource in Codebook).value,"scala",(codebookDecoderPackageName in Codebook).value)
     }
     cachedCompile(((sourceDirectory in Codebook).value ** "*.cb").get.toSet).toSeq
   }
