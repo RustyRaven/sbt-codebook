8a92a8c37d25ddc631c0cf0270b23141fb797ff5
Osamu Takahashi <nitr@sopranoworks.com>
2017-02-10 11:07:29 +0900

    Added clientCode command

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # sbt-codebook
+
+## How to setup
+
+In project/plugins.sbt
+
+```
+resolvers += "RustyRaven" at "http://rustyraven.github.io"
+
+addSbtPlugin("com.rustyraven" % "sbt-codebook" % "1.0-SNAPSHOT")
+```
+
+And add 
+
+```
+enablePlugins(CodebookPlugin)
+```
+
+to your project.
+
+## Commands
+ 
+### generate
+
+Generating the protocol server code.
+It will automatically execute on compile time.
+
+### document
+
+Generating the protocol document.
+
+### skeleton
+
+Generating the api handler skeleton code.
+
+### clientCode
+
+Generating the protocol client code.
+
+## Licence
+
+MIT
--- src/main/scala/com/rustyraven/SbtCodebook.scala
+++ src/main/scala/com/rustyraven/SbtCodebook.scala
@@ -2,8 +2,9 @@ package com.rustyraven
 
 import sbt._
 import Keys._
-import com.rustyraven.codebook.ProtocolGenerator
+import com.rustyraven.codebook.{GeneratorOptions, ProtocolGenerator}
 import sbt.plugins.JvmPlugin
+import sbt.complete.DefaultParsers._
 
 object CodebookPlugin extends AutoPlugin {
   val Codebook = config("codebook")
@@ -11,6 +12,10 @@ object CodebookPlugin extends AutoPlugin {
   object autoImport {
     val codebookGenerate = TaskKey[Seq[File]]("generate")
     val withDocument = SettingKey[Boolean]("Generate Document")
+    val documentSourceDir = SettingKey[String]("Document source directory")
+    val skeleton = InputKey[Unit]("skeleton")
+    val document = TaskKey[Unit]("document")
+    val clientCode = InputKey[Unit]("clientCode")
   }
 
   import autoImport._
@@ -26,9 +31,13 @@ object CodebookPlugin extends AutoPlugin {
   override lazy val projectSettings = inConfig(Codebook)(Seq(
     sourceDirectory <<= (sourceDirectory in Compile) { _ / "codebook"},
     scalaSource <<= (sourceManaged in Compile).apply(_ / "codebook"),
-    codebookGenerate <<= generatorTask
+    codebookGenerate <<= generatorTask,
+    skeleton <<= skeletonTask,
+    document <<= documentTask,
+    clientCode <<= clientCodeTask
   )) ++ Seq(
     withDocument := false,
+    documentSourceDir := "docsrc",
 
     ivyConfigurations += Codebook,
     managedSourceDirectories in Compile <+= (scalaSource in Codebook),
@@ -49,5 +58,34 @@ object CodebookPlugin extends AutoPlugin {
           withDocument.value)
     }
     cachedCompile(((sourceDirectory in Codebook).value ** "*.cb").get.toSet).toSeq
+  }
+
+  def skeletonTask:Def.Initialize[InputTask[Unit]] = Def.inputTask {
+    val args = spaceDelimited("<arg>").parsed
+    val category = args.length match {
+      case 0 =>
+        None
+      case _ =>
+        Some(args.head)
+    }
+    val _sources = (sourceDirectory map (path => (path ** "*.cb").get)).value.toSet
+    ProtocolGenerator.generate(_sources,new File(baseDirectory.value,"skeleton"),"scala",None,GeneratorOptions(false,false,category.forall(_ => false),true,category))
+  }
+
+  def documentTask:Def.Initialize[Task[Unit]] = Def.task {
+    val _sources = (sourceDirectory map (path => (path ** "*.cb").get)).value.toSet
+    ProtocolGenerator.generate(_sources,new File(baseDirectory.value,documentSourceDir.value),"sphinx",None,GeneratorOptions())
+  }
+
+  def clientCodeTask:Def.Initialize[InputTask[Unit]] = Def.inputTask {
+    val args = spaceDelimited("<arg>").parsed
+    val lang = args.length match {
+      case 0 =>
+        "csharp"
+      case _ =>
+        args.head
+    }
+    val _sources = (sourceDirectory map (path => (path ** "*.cb").get)).value.toSet
+    ProtocolGenerator.generate(_sources,new File(baseDirectory.value,s"clientCode/$lang"),lang,Some(new File(baseDirectory.value,documentSourceDir.value)),GeneratorOptions(withClientCode = true,withDocument = true))
   }
 }
