8da80a416b24b21da52dfae88f5bb60199c601fe
Osamu Takahashi <nitr@sopranoworks.com>
2017-07-31 21:51:49 +0900

    Modified organization, added '-'

--- project/SbtCodebookBuild.scala
+++ project/SbtCodebookBuild.scala
@@ -9,14 +9,15 @@ object SbtCodebookBuild extends Build {
 
     settings = Seq(
       name := "sbt-codebook",
-      organization := "com.rustyraven",
+      organization := "com.rusty-raven",
       version := "1.1-SNAPSHOT",
       sbtPlugin := true,
 
-      resolvers ++= Seq("RustyRaven Repository" at "http://rustyraven.github.io"),
+//      resolvers ++= Seq("RustyRaven Repository" at "http://rustyraven.github.io"),
+      resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
       libraryDependencies ++= Seq(
-        "com.rustyraven" %% "codebook" % "1.1-SNAPSHOT"),
+        "com.rusty-raven" %% "codebook" % "1.1-SNAPSHOT")
 
-      publishTo := Some(Resolver.file("sbt-codebook",file("../RustyRaven.github.io"))(Patterns(true, Resolver.mavenStyleBasePattern))))
-    )
+//      publishTo := Some(Resolver.file("sbt-codebook",file("../RustyRaven.github.io"))(Patterns(true, Resolver.mavenStyleBasePattern))))
+    ))
 }
